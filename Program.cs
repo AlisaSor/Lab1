6d49964c256ce48da418a95c1525099f548edc20
Alisa Soroka <aliska.soroka@mail.ru>
2022-01-21 00:03:38 +0600

    Initial project version

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Questionnaire
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(">> Вводите слова, завершая каждое нажатием Enter");
+            Console.WriteLine(">> Для выхода наберите \"exit\"");
+            
+            var word = "";
+            var maxLength = 0;
+            var theBiggestWord = word;
+            do
+            {
+                Console.Write(">> ");
+                word = Console.ReadLine();
+                if (word.Length > maxLength)
+                {
+                    maxLength = word.Length;
+                    theBiggestWord = word.ToUpper();
+                }
+            } while (word != "exit");
+            Console.WriteLine(">>");
+            Console.WriteLine(">> Считывание завершено");
+            Console.WriteLine($"Самое длинное слово: {theBiggestWord} ({ theBiggestWord.Length}) ");
+            Console.ReadKey();
+        }
+    }
+}
+
